7b4e74843c949256a3b87f5ce11cdb58173447cc
vlad <vlad@vlads-MacBook-Pro-2.local>
2021-05-18 15:44:30 -0700

    update docs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # k8s-troposphere
+This repo builds an infra stack in AWS that includes a VPC, subnets, security groups and other networking components as well as an instance which creates a Kubernetes cluster.
+
+Prerequisites:
+
+-python3 installed
+-awscli installed
+-troposphere installed
+-a configured EC2 keypair with its name assigned to the 'KEY' param in run.sh
+
+
+To build the stack run 'run.sh'
--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 IP=$(curl ifconfig.me)
+KEY=awskey
 
 python3 vpc_and_instance.py  > stack.json
 
-aws cloudformation create-stack \
+aws cloudformation update-stack \
 --stack-name teststack \
 --template-body file://./stack.json \
---parameters ParameterKey=KeyName,ParameterValue=awskey ParameterKey=SSHLocation,ParameterValue=$IP/32 \
+--parameters ParameterKey=KeyName,ParameterValue=$KEY ParameterKey=SSHLocation,ParameterValue=$IP/32 \
 --capabilities CAPABILITY_IAM
